e545207c0ab50a80df0096a74781b33a6dbb8aa2
Brady Pierce <bradypierce13@gmail.com>
2022-05-10 17:48:03 -0700

    add vscode config

---
+++ lua/vscode.lua
@@ -0,0 +1,19 @@
+vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap("n", "<C-j>", [[<cmd>call VSCodeNotify('workbench.action.focusBelowGroup')<cr>]], opts)
+keymap("n", "<C-k>", [[<cmd>call VSCodeNotify('workbench.action.focusAboveGroup')<cr>]], opts)
+keymap("n", "<C-h>", [[<cmd>call VSCodeNotify('workbench.action.focusRightGroup')<cr>]], opts)
+keymap("n", "<C-l>", [[<cmd>call VSCodeNotify('workbench.action.focusLeftGroup')<cr>]], opts)
+
+local opts = { silent = true }
+keymap("n", "<S-l>", ":tabn<CR>", opts)
+keymap("n", "<S-h>", ":tabp<CR>", opts)
+keymap("n", "<Space>ff", ":find<CR>", opts)
+keymap("n", "<Space>c", ":quit<CR>", opts)
